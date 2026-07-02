f119e736b48b37f75f216899d8e82ce62b02109c
VedantGharat7 <gharatvedant2@gmail.com>
2026-07-02 23:20:10 +0530

    added largest value

--- arraysmallest.cpp
+++ arraysmallest.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 int main(){
-int i,n,max;
+int i,n;
 
 int size =5;
 int nums [] = {15,1,24,14,29};
 int smallest = INT_MAX;
+int largest = INT_MIN;
 for (int i = 0; i < size; i++)
 {
     // if (nums[i]< smallest) Method 1
     // {smallest = nums[i];
        smallest = min(nums[i],smallest);
+       largest = max(nums[i],largest);
     }
     
-cout << "smallest value = " << smallest;
+cout << "smallest value = " << smallest << endl;
+cout << "Largest value = " << largest;
 
 
 
